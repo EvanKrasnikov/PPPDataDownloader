1ed6ff798313af4a867ad65d2c94fcec24fa0665
i.krasnikov <geographer29@yandex.ru>
2018-05-24 19:06:58 +0300

    1 bug fixed

--- src/main/java/pppdatadownloader/utils/Range.java
+++ src/main/java/pppdatadownloader/utils/Range.java
@@ -4,7 +4,7 @@ public class Range {
     public static int[] range(int begin, int end){
         int[] values = new int[end - begin];
 
-        for (int i = 0; i < begin - end; i++) {
+        for (int i = 0; i < end - begin; i++) {
             values[i] = begin++;
         }
         return values;
