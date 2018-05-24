771101db50b8d5fecd8e25add80ca666319a202c
i.krasnikov <geographer29@yandex.ru>
2018-05-24 19:23:05 +0300

    FileNameBuilder class allows to form a list of chosen data

---
+++ src/main/java/pppdatadownloader/utils/FileNameBuilder.java
@@ -0,0 +1,31 @@
+package pppdatadownloader.utils;
+
+import pppdatadownloader.RinexHeader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static pppdatadownloader.utils.DateConverter.getDaysCount;
+import static pppdatadownloader.utils.DateConverter.getWeekNumber;
+import static pppdatadownloader.utils.DateConverter.getWeekNumberAndDay;
+
+public class FileNameBuilder {
+    private List<String> names = new ArrayList<>();
+
+    //returns list of filenames for each selected rinex
+    public FileNameBuilder addPreciseEphemeresis(List<RinexHeader> rinexes) throws Exception{
+        for(RinexHeader rinex: rinexes){
+            int weeks = getWeekNumber(rinex.getFirstObs());
+            int days = getWeekNumberAndDay(rinex.getFirstObs());
+
+            for (int i = 0; i < getDaysCount(rinex) ; i++) {
+                if (days > 7){
+                    days = days - 7;
+                    weeks++;
+                }
+                names.add("/pub/products/" + weeks + "/igr" + days + ".sp3.Z");
+            }
+        }
+        return this;
+    }
+}
