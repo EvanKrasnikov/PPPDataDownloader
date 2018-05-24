0cc30a7dbdf10230fe88c687a594c5c3bf955c7b
i.krasnikov <geographer29@yandex.ru>
2018-05-24 16:53:19 +0300

    DateConverter class refactored

--- src/main/java/pppdatadownloader/utils/DateConverter.java
+++ src/main/java/pppdatadownloader/utils/DateConverter.java
@@ -3,29 +3,26 @@ package pppdatadownloader.utils;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class DateConverter {
-    public static final String REFERENCE_DATE  = "19800106";
+    private static long REFERENCE_TIME;
 
-    private static Date getDate(String data) throws ParseException{
-        return new SimpleDateFormat("yyyyMMdd").parse(data);
+    public DateConverter() throws ParseException {
+        REFERENCE_TIME = new SimpleDateFormat("yyyyMMdd")
+                .parse("19800106")
+                .getTime();
     }
 
-    public static Date getDate(String[] elements){
+    public static Date getDate(List<String> elements) throws ParseException{
         StringBuilder stringBuilder = new StringBuilder();
-        Date date = new Date();
 
         for (String element: elements){
             element = validate(element);
             stringBuilder.append(element);
         }
 
-        try {
-            date = new SimpleDateFormat("yyyyMMddhhmmss").parse(stringBuilder.toString());
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return date;
+        return new SimpleDateFormat("yyyyMMddhhmmss").parse(stringBuilder.toString());
     }
 
     //get day number from the beginning of the year
@@ -36,7 +33,7 @@ public class DateConverter {
 
     //returns week number from 1980-01-01 and the day of the week
     public static int getWeekNumberAndDay(Date date) throws ParseException{
-        long difference = date.getTime() - getDate(REFERENCE_DATE).getTime();
+        long difference = date.getTime() - REFERENCE_TIME;
         int days = (int)(difference/(24 * 60 * 60 * 1000));
         return (days / 7) + (days % 7 - 1);
     }
