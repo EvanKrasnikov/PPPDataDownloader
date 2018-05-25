a0c1a94ea06c53c044cd9249ce80418231ffb019
i.krasnikov <geographer29@yandex.ru>
2018-05-25 14:22:15 +0300

    Added few methods to count days and weeks

--- src/main/java/pppdatadownloader/utils/DateConverter.java
+++ src/main/java/pppdatadownloader/utils/DateConverter.java
@@ -1,10 +1,12 @@
 package pppdatadownloader.utils;
 
+import pppdatadownloader.RinexHeader;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+
+import static pppdatadownloader.utils.Range.range;
 
 public class DateConverter {
     private static long REFERENCE_TIME;
@@ -15,14 +17,10 @@ public class DateConverter {
                 .getTime();
     }
 
-    public static List<Date> getDates(List<String> elements) throws ParseException{
-        List<Date> result = new ArrayList<>();
-        for (String element: elements){
-            element = validate(element);
-            result.add(new SimpleDateFormat("yyyyMMddhhmmss")
-                    .parse(element));
-        }
-        return result;
+    //returns date from rinex string
+    public static Date getDate(String element) throws ParseException{
+        return new SimpleDateFormat("yyyyMMddhhmmss")
+                .parse(validate(element));
     }
 
     //get day number from the beginning of the year
@@ -45,8 +43,29 @@ public class DateConverter {
         return (days / 7);
     }
 
+    //add zero if it absents
     private static String validate(String check){
         if (check.length() == 1) return 0 + check;
         else return check;
+    }
+
+    //returns an array of observation days numbers
+    public static int[] getDaysNumbers(RinexHeader rinex) throws ParseException{
+        return range(getDayNumber(rinex.getLastObs()), getDayNumber(rinex.getFirstObs()));
+    }
+
+    //returns an array of observation weeks numbers
+    public static int[] getWeeksNumbers(RinexHeader rinex) throws ParseException{
+        return range(getWeekNumberAndDay(rinex.getLastObs()), getWeekNumberAndDay(rinex.getFirstObs()));
+    }
+
+    //returns how much days count from first obs
+    public static int getDaysCount(RinexHeader rinex) throws ParseException{
+        return getDayNumber(rinex.getLastObs()) - getDayNumber(rinex.getFirstObs());
+    }
+
+    //returns how much weeks count from first obs
+    public static int getWeeksCount(RinexHeader rinex) throws ParseException{
+        return getWeekNumber(rinex.getLastObs()) - getWeekNumber(rinex.getFirstObs());
     }
 }
