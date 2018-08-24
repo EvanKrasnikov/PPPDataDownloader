3c72b041bdb2d70fbf3bb0eb1e424723141a0dc8
i.krasnikov <geographer29@yandex.ru>
2018-08-24 17:17:01 +0300

    Added App class

---
+++ src/main/java/pppdatadownloader/App.java
@@ -0,0 +1,11 @@
+package pppdatadownloader;
+
+import pppdatadownloader.ui.Window;
+
+public class App {
+    public static void main(String[] args) {
+        Window app = new Window();
+        app.setVisible(true);
+        app.setResizable(false);
+    }
+}
