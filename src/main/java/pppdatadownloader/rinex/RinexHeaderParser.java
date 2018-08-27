6c685dae986d68c7d141d547e8a434e37b139faa
i.krasnikov <geographer29@yandex.ru>
2018-08-27 18:54:32 +0300

    Class refactored. Added javadoc

--- src/main/java/pppdatadownloader/rinex/RinexHeaderParser.java
+++ src/main/java/pppdatadownloader/rinex/RinexHeaderParser.java
@@ -1,7 +1,5 @@
 package pppdatadownloader.rinex;
 
-import pppdatadownloader.rinex.RinexHeader;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -16,14 +14,19 @@ import java.util.regex.Pattern;
 import static pppdatadownloader.utils.DateConverter.getDate;
 import static pppdatadownloader.utils.DateConverter.getDayNumber;
 import static pppdatadownloader.utils.DateConverter.getWeekNumberAndDay;
-import static pppdatadownloader.rinex.ParseMap.getParseInstruction;
 
 public class RinexHeaderParser {
     private static final int BUFFER_SIZE = 4096;
     private RinexHeader rinex = new RinexHeader();
     private String[] headerLines;
 
-    public void parse(File file) throws IOException, ParseException {
+    /**
+     * Method used for reading headers from RINEX files
+     * @param file
+     * @throws IOException
+     * @throws ParseException
+     */
+    public void readHeader(File file) throws IOException, ParseException {
         RandomAccessFile randomAccessFile = new RandomAccessFile(file,"r");
         FileChannel channel = randomAccessFile.getChannel();
         ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
@@ -31,27 +34,14 @@ public class RinexHeaderParser {
         channel.read(buffer);
         buffer.flip();
         CharBuffer charBuffer = charset.decode(buffer);
-        headerLines = charBuffer.toString().split("\\n");;
+        headerLines = charBuffer.toString().split("\\n");
         read();
     }
 
-    private String parseObs(String match, String mapKey){
-        String regexp = "\\s+([#/.\\w]+)";
-        Pattern pattern = Pattern.compile(regexp);
-        Matcher matcher = pattern.matcher(match);
-        StringBuilder stringBuilder = new StringBuilder();
-
-        if (matcher.find()){
-            int[] expression = getParseInstruction(mapKey);
-            for (int i = 0; i < expression.length ; i++) {
-                if (i == expression[i]) stringBuilder.append(matcher.group(1));
-            }
-        } else {
-            System.out.println("Find nothing!");
-        }
-        return stringBuilder.toString();
-    }
-
+    /**
+     * Method used for defining what regexp to use
+     * @throws ParseException
+     */
     private void read() throws ParseException{
         String regexp = "[/#\\w]+(\\s{1}[/#\\w]+)+$";
         Pattern pattern = Pattern.compile(regexp);
@@ -60,15 +50,46 @@ public class RinexHeaderParser {
             matcher = pattern.matcher(line);
 
             if (matcher.find()) {
-                String data = parseObs(line, matcher.group(0));
+                String data;
+
                 switch (matcher.group(0)){
-                    case "TIME OF FIRST OBS" : rinex.setFirstObs(getDate(data));
-                    case "TIME OF LAST OBS" : rinex.setLastObs(getDate(data));
+                    case "TIME OF FIRST OBS" : {
+                        data = parse(line,"TIME OF FIRST OBS");
+                        rinex.setFirstObs(getDate(data));
+                    }
+
+                    case "TIME OF LAST OBS" : {
+                        data = parse(line,"TIME OF LAST OBS");
+                        rinex.setLastObs(getDate(data));
+                    }
                 }
             }
         }
     }
 
+    /**
+     * Method returns a String appended from each group of pattern that match regexp
+     * @param line
+     * @param regex
+     * @return String
+     */
+    private String parse(String line, String regex){
+        Pattern pattern = Pattern.compile(ParseMap.getRegExp(regex));
+        Matcher matcher = pattern.matcher(line);
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while (matcher.find()){
+            sb.append(matcher.group(++count));
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Method prints information about corresponded instance of RINEX - header class
+     * @throws ParseException
+     */
     public void print() throws ParseException{
         System.out.println(rinex.toString());
         System.out.println("DayNum " + getDayNumber(rinex.getFirstObs()));
